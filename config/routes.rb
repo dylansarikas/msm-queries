9e1b2f65db69da9db98f7e44975df15bce88eb3d
dylansarikas <dylan.sarikas@gmail.com>
2024-06-10 14:48:27 -0500

    adds models, directors index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  #get 'directors/index'
   get("/", { :controller => "misc", :action => "homepage" })
+  get "/directors" => "directors#index"
+  get "/directors/:id" => "directors#show"
 end
